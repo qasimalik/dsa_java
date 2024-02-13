71dfe8103b3ee79e0c025c9ac05b8b153036a71a
qasimalik <qsmmlk12@gmail.com>
2024-02-13 15:09:26 +0530

    second

--- Data Structures and Algorithms/src/Stack/infin.java
+++ Data Structures and Algorithms/src/Stack/infin.java
@@ -42,10 +42,35 @@ public class infin {
         return postfix.toString();
     }
 
+    static boolean isOperand(char x){
+        return (x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z');
+    }
+
+    static String getInfin(String str){
+        Stack <String> s = new Stack<String>();
+        int l = str.length();
+        for (int i = 0; i < l; i++) {
+            char x = str.charAt(i);
+            if(isOperand(x)){
+                s.push(x+ "");
+            } else {
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+                s.push("(" + op2 + x + op1 + ")");
+            }
+        }
+        return s.peek();
+    }
+
     public static void main(String[] args) {
         String infixExpression = "a+b*(c^d-e)^(f+g*h)-i";
         String postfixExpression = infixToPostfix(infixExpression);
         System.out.println("Infix Expression: " + infixExpression);
         System.out.println("Postfix Expression: " + postfixExpression);
+
+        String infixExp = getInfin("ab*c+");
+        System.out.println("Infix Expression: " + infixExp);
     }
 }
